09fb19e73e6612e25c817be2023d9eeb07dd5693
Entropie67 <34799718+Entropie67@users.noreply.github.com>
2021-01-14 11:12:12 +0100

    Update constantes.py
    constantes

--- constantes.py
+++ constantes.py
@@ -4,7 +4,7 @@ PATH = "/home/raph/PY/Laby"
 BLUE = (0, 120, 180)
 CONTINUER = True
 
-
+# Ici tu pourrais passer en majusules les constantes
 Crapeaux = PATH+"/assets/crapeaux.png"
 Wall = PATH+"/assets/mur.jpg"
 Fond = PATH+"/assets/fond.png"
